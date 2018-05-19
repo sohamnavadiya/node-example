e83c468d7f5049de6670abecc49af21aa2392c26
soham <som.insomniacs@gmail.com>
2018-05-19 18:51:16 +0530

    added more API

--- node-server/server.js
+++ node-server/server.js
@@ -3,7 +3,25 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hello Express!");
+    res.send({
+        name: 'Soham',
+        age: 24,
+        likes: [{
+                facebook: 12,
+                id: "FB@123"
+            },{
+                orkut: 123,
+                id: "OK@123"
+            }]
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
+app.get('/bad', (req, res) => {
+    errorMessage: 'Unable to handle request'
 });
 
 app.listen(3000);
